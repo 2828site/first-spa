8f9c8dd5b925430692031a550412cbec7b9458de
demo <115445178+2828site@users.noreply.github.com>
2022-10-10 13:27:35 +0800

    V2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React<br>
+          Learn React<br/>
           demo 2828site
         </a>
       </header>
